01e4ae716422da2b256f65884a34d98a7be74533
Hydenaky <juanpi.cn2004@gmail.com>
2026-02-01 15:08:15 -0500

    before big changes

--- src/app/presentation/pages/safirial-icons/safirial-icons.ts
+++ src/app/presentation/pages/safirial-icons/safirial-icons.ts
@@ -1,15 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CodeConsole } from '../catarina/components/docs-examples/code-console/code-console';
-import { ButtonIconsExample } from '../catarina/components/docs-examples/example-components/button/button-icons-example.component';
+import { iconList } from 'safirial-icons';
+import { Icon } from 'catarina';
+import { IconSizesExample } from '../catarina/components/docs-examples/example-components/icon/icon-sizes-example.component';
 
 @Component({
   selector: 'app-safirial-icons',
-  imports: [CodeConsole, CommonModule],
+  imports: [CodeConsole, CommonModule, Icon],
   templateUrl: './safirial-icons.html',
   styleUrl: './safirial-icons.scss'
 })
 export class SafirialIcons {
   // Simplemente pasar la clase del componente
-  buttonIconsComponent = ButtonIconsExample;
+  iconSizesExample = IconSizesExample;
+  iconList = iconList;
 }

1f2c809973e4c6b0a71d65140f1bd74ed09c5606
Hydenaky <juanpi.cn2004@gmail.com>
2026-02-11 02:20:36 -0500

    mobile improves

--- src/app/presentation/pages/portfolio/components/organisms/profile/molecules/profile-basic-data/profile-basic-data.ts
+++ src/app/presentation/pages/portfolio/components/organisms/profile/molecules/profile-basic-data/profile-basic-data.ts
@@ -8,36 +8,8 @@ import languages from './profile-basic-data-languages.json';
 @Component({
   selector: 'mol-profile-basic-data',
   imports: [CommonModule],
-  template: `
-      <div style="text-align: center;">
-        <h1>Juan Pablo Canon</h1>
-        <h4 style="font-size: 2.4em;">{{title | titlecase}}</h4>
-    </div>
-    <div class="line"></div>
-  `,
-  styles: `
-  .line {
-    width: 100%;
-    height: .3em;
-    background-color: var(--neutral-color-1);
-  }
-  
-  h1,
-  h4 {
-    margin: 0;
-    padding: 0;
-    margin-bottom: 0.3em;
-    line-height: 1em;
-
-    color: var(--neutral-color-9);
-    
-      font-family: "Roboto Condensed", sans-serif;
-      font-optical-sizing: auto;
-      font-weight: bold;
-      font-size: 3em;
-      font-style: normal;
-    }
-  `
+  templateUrl: 'profile-basic-data.html',
+  styles: ``
 })
 export class ProfileBasicData implements OnInit, OnDestroy {
   title?: string;
